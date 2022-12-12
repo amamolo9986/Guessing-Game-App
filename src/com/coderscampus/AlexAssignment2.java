cf11fabee3a0982855aca653563b2a6a56d1882d
amamolo9986 <amamolo9986@yahoo.com>
2022-12-12 11:37:52 -0600

    Final AlexAssignment2

--- src/com/coderscampus/AlexAssignment2.java
+++ src/com/coderscampus/AlexAssignment2.java
@@ -12,24 +12,24 @@ public class AlexAssignment2 {
 		int randomNumber = random.nextInt(101);
 
 		System.out.println("Guess a number between 1 and 100");
-		//System.out.println(randomNumber);
+		System.out.println(randomNumber);
 
 		int numberOfGuesses = 0;
 		while (numberOfGuesses < 5) {
 			int guess = scan.nextInt();
 			numberOfGuesses++;
 
-			if (guess < 1 || guess > 100 && guess == numberOfGuesses--) {
+			if (guess < 1 || guess > 100 && guess == numberOfGuesses) {
 				System.out.println("That number is not between 1 and 100. Please try again.");
 			} else if (guess < randomNumber && numberOfGuesses < 5) {
 				System.out.println("Pick a higher number");
 			} else if (guess > randomNumber && numberOfGuesses < 5) {
 				System.out.println("Pick a lower number");
-			} else if (numberOfGuesses == 5) {
+			} else if (guess == 5) {
 				System.out.println("You lose! The number was " + randomNumber);
 			}
 
-			if (guess == randomNumber && numberOfGuesses < 5) {
+			if (guess == randomNumber && numberOfGuesses <= 5) {
 				System.out.println("You win!");
 				break;
 			}
