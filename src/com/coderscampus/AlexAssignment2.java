94a2f4e550eba5330f626a641e1281b1adf38ba8
amamolo9986 <amamolo9986@yahoo.com>
2023-02-24 08:28:49 -0600

    refactor

--- src/com/coderscampus/AlexAssignment2.java
+++ src/com/coderscampus/AlexAssignment2.java
@@ -9,7 +9,13 @@ public class AlexAssignment2 {
 
 		Scanner scan = new Scanner(System.in);
 		Random random = new Random();
-		int randomNumber = random.nextInt(101);
+		int randomNumber = random.nextInt(100) + 1; 
+		
+		// the reason that this isnt (101) is because that means 0-100 - and we can't
+		// have the random number generate as 0. So to fix this we simply add +1
+		// after the parenthesis, this means add one to the starting number.
+		// and since we've added 1, we change the number in the parentheses from
+		// 101 to 100
 
 		System.out.println("Guess a number between 1 and 100");
 		System.out.println(randomNumber);
@@ -17,23 +23,25 @@ public class AlexAssignment2 {
 		int numberOfGuesses = 0;
 		while (numberOfGuesses < 5) {
 			int guess = scan.nextInt();
-			numberOfGuesses++;
 
 			if (guess < 1 || guess > 100 && guess != numberOfGuesses) {
 				System.out.println("That number is not between 1 and 100. Please try again.");
-				numberOfGuesses --;
-			} else if (guess < randomNumber && numberOfGuesses < 5) {
-				System.out.println("Pick a higher number");
-			} else if (guess > randomNumber && numberOfGuesses < 5) {
-				System.out.println("Pick a lower number");
-			} else if (numberOfGuesses== 5) {
-				System.out.println("You lose! The number was " + randomNumber);
-			}
-
-			if (guess == randomNumber && numberOfGuesses <= 5) {
-				System.out.println("You win!");
-				break;
+				continue;
+			} else {
+				numberOfGuesses++;
+				if (guess == randomNumber) {
+					System.out.println("You win!");
+					break;
+				} else if (guess < randomNumber && numberOfGuesses < 5) {
+					System.out.println("Pick a higher number");
+				} else if (guess > randomNumber && numberOfGuesses < 5) {
+					System.out.println("Pick a lower number");
+				}
+				if (numberOfGuesses == 5) {
+					System.out.println("You lose! The number was " + randomNumber);
+				}
 			}
 		}
+		scan.close();
 	}
 }
